ba88bedb8e1b2e10cd05b4ef4ed0f86fd9f9344a
velykitty <jiyunee13@gmail.com>
2020-08-15 11:46:31 +0900

    testcode

---
+++ src/main/java/com/sweet/kiri/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.sweet.kiri.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
