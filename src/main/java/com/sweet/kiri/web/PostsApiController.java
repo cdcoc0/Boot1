9d22a711dac92f0ffe924f36d498e6097d61ebe2
velykitty <jiyunee13@gmail.com>
2020-08-18 18:33:57 +0900

    jpa, testcodes again

---
+++ src/main/java/com/sweet/kiri/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.sweet.kiri.web;
+
+import com.sweet.kiri.service.posts.PostsService;
+import com.sweet.kiri.web.dto.PostsResponseDto;
+import com.sweet.kiri.web.dto.PostsSaveRequestDto;
+import com.sweet.kiri.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
